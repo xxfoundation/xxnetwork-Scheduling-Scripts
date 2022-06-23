67c80cb325036f9cc9882eee9ad55132e00a6bea
jbhusson <jonah@elixxir.io>
2022-06-23 17:52:06 -0400

    Points should maintain an external list of wallet to country mappings

--- points.py
+++ points.py
@@ -28,6 +28,8 @@ state_key = "consensus_points_timestamp"  # Static key used for states table
 positive_points_func = 'submit_cmix_points'
 negative_points_func = 'submit_cmix_deductions'
 raw_points_log = ''
+wallet_list_path = ''
+wallet_list = None
 
 
 #################
@@ -126,6 +128,8 @@ def process_period(conn, substrate, keypair, point_info, start_period, end_perio
 
     # Calculate points for the retrieved round information
     wallet_points, raw_points = round_point_computation(point_info, round_info, active_nodes)
+
+    update_wallet_country_list(active_nodes, wallet_list_path)
 
     # Define Lists
     positive = []
@@ -412,7 +416,7 @@ def get_args():
     get_args controls the argparse usage for the script.  It sets up and parses
     arguments and returns them in dict format
     """
-    global raw_points_log
+    global raw_points_log, wallet_list_path
     parser = argparse.ArgumentParser(description="Options for point assignment script")
     parser.add_argument("--verbose", action="store_true",
                         help="Print debug logs", default=False)
@@ -441,6 +445,7 @@ def get_args():
                         default="cmix")
     parser.add_argument("--pass", type=str,
                         help="DB password")
+    parser.add_argument("--wallet-list-path", type=str, help="List of wallet-country")
 
     args = vars(parser.parse_args())
     log.basicConfig(format='[%(levelname)s] %(asctime)s: %(message)s',
@@ -448,6 +453,7 @@ def get_args():
                     datefmt='%d-%b-%y %H:%M:%S',
                     filename=args["log"])
     raw_points_log = args['raw_points_log']
+    wallet_list_path = args['wallet_list_path']
     return args
 
 
@@ -472,6 +478,21 @@ def parse_bins(countries_list):
         country = bytes.fromhex(elem[0][2:]).decode('utf-8')
         ret[country] = (elem[1])
     return ret
+
+
+def update_wallet_country_list(active_nodes, list_path):
+    global wallet_list
+    with open(list_path, "a+") as f:
+        if not wallet_list:
+            wallet_list = {}
+            lines = f.readlines()
+            for line in lines:
+                parts = line.split(",")
+                wallet_list[parts[0]] = wallet_list[parts[1]]
+        for line in active_nodes:
+            if line[0] not in wallet_list:
+                wallet_list[line[0]] = line[2]
+                f.write(f"{line[0]},{line[2]}\n")
 
 
 ########################
